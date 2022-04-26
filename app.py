2888402d45ee08d5842f750ecf7668fe1f23909d
Ethan Pinto <ethanpintoa@gmail.com>
2022-04-25 20:16:13 -0400

    Update database url env var

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('CLEARDB_DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
